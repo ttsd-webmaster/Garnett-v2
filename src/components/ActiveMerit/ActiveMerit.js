bacaffc25b98b0163027c69f8b49e9aa23249748
Kaiser <kaiserkim167@gmail.com>
2017-11-14 02:15:06 -0800

    Added merit button to Active Merit Book

--- src/components/ActiveMerit/ActiveMerit.js
+++ src/components/ActiveMerit/ActiveMerit.js
@@ -17,8 +17,7 @@ const listItemStyle = {
 };
 
 const avatarStyle = {
-  top: 8,
-  boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 10px, rgba(0, 0, 0, 0.23) 0px 3px 10px'
+  top: 8
 };
 
 const dividerStyle = {
