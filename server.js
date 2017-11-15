3e613f31f8f9799296852494f669c91470f0f00c
Kaiser <kaiserkim167@gmail.com>
2017-11-14 23:09:39 -0800

    Changed server.js

--- server.js
+++ server.js
@@ -27,17 +27,10 @@ admin.initializeApp({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './client/build')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  })
-}
-
-app.use(express.static(path.join(__dirname, './client/public')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/public/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 // Login Get Route
