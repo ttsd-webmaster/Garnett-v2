6fe06ee382df11031e30ef2338a0631bae8dc120
Kaiser <kaiserkim167@gmail.com>
2017-11-23 04:05:33 -0800

    Added react-lazy-load for lazy loading contacts

--- client/src/components/Contacts/ActiveList.js
+++ client/src/components/Contacts/ActiveList.js
@@ -1,6 +1,7 @@
 import './Contacts.css';
 
 import React, {Component} from 'react';
+import LazyLoad from 'react-lazy-load';
 import Avatar from 'material-ui/Avatar';
 import {ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
@@ -26,27 +27,29 @@ export default class ActiveList extends Component {
     return (
       this.props.activeArray.map((active, i) => (
         this.props.classLabel === active.class &&
-          <div key={i}>
-            <Divider style={dividerStyle} inset={true} />
-            <ListItem
-              innerDivStyle={listItemStyle}
-              leftAvatar={<Avatar size={70} src={active.photoURL} style={avatarStyle} />}
-              primaryText={
-                <p className="active-name"> {active.firstName} {active.lastName}</p>
-              }
-              secondaryText={
-                <p>
-                  {active.year}
-                  <br />
-                  {active.major}
-                </p>
-              }
-              secondaryTextLines={2}
-              onClick={() => this.props.handleOpen(active)}
-            >
-            </ListItem>
-            <Divider style={dividerStyle} inset={true} />
-          </div>
+          <LazyLoad height={88} key={i}>
+            <div>
+              <Divider style={dividerStyle} inset={true} />
+              <ListItem
+                innerDivStyle={listItemStyle}
+                leftAvatar={<Avatar size={70} src={active.photoURL} style={avatarStyle} />}
+                primaryText={
+                  <p className="active-name"> {active.firstName} {active.lastName}</p>
+                }
+                secondaryText={
+                  <p>
+                    {active.year}
+                    <br />
+                    {active.major}
+                  </p>
+                }
+                secondaryTextLines={2}
+                onClick={() => this.props.handleOpen(active)}
+              >
+              </ListItem>
+              <Divider style={dividerStyle} inset={true} />
+            </div>
+          </LazyLoad>
       ))
     )
   }
