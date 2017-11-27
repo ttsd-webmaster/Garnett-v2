6a0385156b213c4c8464d05967705b9c4543c5d0
Kaiser <kaiserkim167@gmail.com>
2017-11-27 04:15:08 -0800

    Added touch action to pledge app

--- client/src/containers/PledgeApp/PledgeApp.js
+++ client/src/containers/PledgeApp/PledgeApp.js
@@ -146,6 +146,18 @@ export default class PledgeApp extends Component {
     else {
       swipeableViewStyle.marginBottom = 0;
     }
+
+    window.onscroll = () => {
+      let view = document.querySelector('.react-swipeable-view-container');
+      let index = this.state.slideIndex;
+
+      if (window.pageYOffset >= 1) {
+        view.childNodes[index].style.touchAction = 'auto';
+      } 
+      else {
+        view.childNodes[index].style.touchAction = 'pan-down';
+      }
+    };
   }
 
   // Changes view margin if view is pledge merit book
