ad893ac8212521f7cf5c3fccb1fa317117df1a43
Kaiser <kaiserkim167@gmail.com>
2018-03-15 23:24:19 -0700

    Updated firebase

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,8 +1,8 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.10.1/firebase-messaging.js');
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
 // Create Workbox service worker instance
