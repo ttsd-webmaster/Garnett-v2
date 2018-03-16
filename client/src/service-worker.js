f0755566f493afcb81399f45bf26f6512b5c309e
Kaiser <kaiserkim167@gmail.com>
2018-03-16 02:26:13 -0700

    Testing workbox

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,8 +1,8 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/4.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.10.1/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/4.10.1/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/4.10.1/firebase-messaging.js');
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
 // Create Workbox service worker instance
@@ -98,23 +98,23 @@ workbox.routing.registerRoute(
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
-firebase.initializeApp({
-  'messagingSenderId': '741733387760'
-});
+// firebase.initializeApp({
+//   'messagingSenderId': '741733387760'
+// });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
+// // Retrieve an instance of Firebase Messaging so that it can handle background
+// // messages.
+// const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log(payload);
-  // Customize notification here
-  const title = 'Garnett';
-  const options = {
-    body: 'Background Message body.',
-    icon: '/images/garnett.png',
-    click_action: 'https://garnett-app.herokuapp.com'
-  };
+// messaging.setBackgroundMessageHandler(function(payload) {
+//   console.log(payload);
+//   // Customize notification here
+//   const title = 'Garnett';
+//   const options = {
+//     body: 'Background Message body.',
+//     icon: '/images/garnett.png',
+//     click_action: 'https://garnett-app.herokuapp.com'
+//   };
 
-  return self.registration.showNotification(title, options);
-});
+//   return self.registration.showNotification(title, options);
+// });
