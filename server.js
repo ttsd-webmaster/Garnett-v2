3672b67569e43da9e2a96b56dd6db8a1f01a0fcb
Kaiser <kaiserkim167@gmail.com>
2019-01-14 20:52:59 -0800

    Created env vars for serviceAccountKey.json and removed file

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 const equal = require('deep-equal');
 const firebase = require('@firebase/app').firebase;
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 const indexRouter = require('./routes/index');
 const meritRouter = require('./routes/merit');
 const chalkboardRouter = require('./routes/chalkboard');
@@ -29,7 +28,18 @@ firebase.initializeApp({
 });
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+  }),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
