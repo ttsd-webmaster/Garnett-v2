a63a5ffc59d2545439e4f09efdb581fffc78d5fb
Kaiser <kaiserkim167@gmail.com>
2019-04-10 05:53:15 -0700

    Deleted old merit dialogs

--- client/src/containers/PledgeApp/components/Sidebar/Dialogs/index.js
+++ client/src/containers/PledgeApp/components/Sidebar/Dialogs/index.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import Loadable from 'react-loadable';
 
-export const LoadablePledgeMeritDialog = Loadable({
-  loader: () => import('./PledgeMeritDialog'),
-  render(loaded, props) {
-    let Component = loaded.default;
-    return <Component {...props}/>;
-  },
-  loading() {
-    return <div></div>
-  }
-});
-
-export const LoadableActiveMeritDialog = Loadable({
-  loader: () => import('./ActiveMeritDialog'),
-  render(loaded, props) {
-    let Component = loaded.default;
-    return <Component {...props}/>;
-  },
-  loading() {
-    return <div></div>
-  }
-});
-
 export const LoadableMeritDialog = Loadable({
   loader: () => import('./MeritDialog'),
   render(loaded, props) {
