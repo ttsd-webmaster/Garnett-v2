71d6eae11f622bf568b64af1893a85310efa75a0
Kaiser <kaiserkim167@gmail.com>
2019-04-13 13:04:28 -0700

    Added Tau to Contacts Class filter

--- client/src/containers/PledgeApp/components/Contacts/data.js
+++ client/src/containers/PledgeApp/components/Contacts/data.js
@@ -1,34 +1,30 @@
 export default {
-   class: [ 'Charter', 'Alpha', 'Beta', 'Gamma', 
-            'Delta', 'Epsilon', 'Zeta', 'Eta', 
-            'Theta', 'Iota', 'Kappa', 'Lambda', 
-            'Mu', 'Nu', 'Xi', 'Omicron', 
-            'Pi', 'Rho', 'Sigma'
-          ],
-   active: ['Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau'],
-   major: [ 'Aerospace Engineering',
-            'Bioengineering',
-            'Chemical Engineering',
-            'Computer Engineering',
-            'Computer Science',
-            'Cognitive Science',
-            'Data Science',
-            'Electrical Engineering',
-            'Environmental Engineering',
-            'Math-Computer Science',
-            'Mechanical Engineering',
-            'Nanoengineering',
-            'Structural Engineering'
-           ],
-   name: [ 'A','B','C','D','E','F','G','H','I','J','K','L','M',
-           'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-         ],
-   year: ['1st Year', '2nd Year', '3rd Year', '4th Year', '5th Year', 'Alumni'],
-   mbti: [ 'INTJ', 'INTP', 'ENTJ', 'ENTP',
-           'INFJ', 'INFP', 'ENFJ', 'ENFP',
-           'ISTJ', 'ISTP', 'ESTJ', 'ESTP',
-           'ISFJ', 'ISFP', 'ESFJ', 'ESFP'
-         ]
+  class: [
+    'Charter', 'Alpha', 'Beta', 'Gamma',
+    'Delta', 'Epsilon', 'Zeta', 'Eta',
+    'Theta', 'Iota', 'Kappa', 'Lambda',
+    'Mu', 'Nu', 'Xi', 'Omicron',
+    'Pi', 'Rho', 'Sigma', 'Tau'
+  ],
+  active: ['Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau'],
+  major: [
+    'Aerospace Engineering', 'Bioengineering', 'Chemical Engineering',
+    'Computer Engineering', 'Computer Science', 'Cognitive Science',
+    'Data Science', 'Electrical Engineering', 'Environmental Engineering',
+    'Math-Computer Science', 'Mechanical Engineering', 'Nanoengineering',
+    'Structural Engineering'
+  ],
+  name: [
+    'A','B','C','D','E','F','G','H','I','J','K','L','M',
+    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+  ],
+  year: ['1st Year', '2nd Year', '3rd Year', '4th Year', '5th Year', 'Alumni'],
+  mbti: [
+    'INTJ', 'INTP', 'ENTJ', 'ENTP',
+    'INFJ', 'INFP', 'ENFJ', 'ENFP',
+    'ISTJ', 'ISTP', 'ESTJ', 'ESTP',
+    'ISFJ', 'ISFP', 'ESFJ', 'ESFP'
+  ]
 };
 
 export type Filter =
