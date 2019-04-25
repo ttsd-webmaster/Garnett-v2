06a38cc0036f2569e9393ef08938566864d7b1c0
Kaiser <kaiserkim167@gmail.com>
2019-04-24 17:49:17 -0700

    Created custom PullToRefresh component

--- client/src/containers/PledgeApp/PledgeApp.js
+++ client/src/containers/PledgeApp/PledgeApp.js
@@ -3,12 +3,15 @@
 import './MobilePledgeApp.css';
 import './PledgeApp.css';
 import { isMobile, configureThemeMode } from 'helpers/functions';
+import { PullToRefreshSpinner } from 'helpers/loaders';
 import { MobileHeader, MobileNavbar } from './components/Mobile';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { Main } from './components/Main/Main';
 import type { User } from 'api/models';
 
 import React, { Fragment, PureComponent } from 'react';
+import pullToRefresh from 'mobile-pull-to-refresh';
+import ptrAnimatesMaterial from 'mobile-pull-to-refresh/dist/styles/material/animates';
 
 type Props = {
   history: RouterHistory,
@@ -21,7 +24,21 @@ export class PledgeApp extends PureComponent<Props> {
   componentDidMount() {
     localStorage.setItem('route', 'pledge-app');
     if (isMobile()) {
+      const pledgeAppContainer = document.getElementById('pledge-app-container');
       configureThemeMode();
+      if (pledgeAppContainer) {
+        pullToRefresh({
+          container: pledgeAppContainer,
+          animates: ptrAnimatesMaterial,
+          refresh() {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                window.top.location.reload()
+              }, 1000);
+            })
+          }
+        });
+      }
     }
   }
 
@@ -33,6 +50,7 @@ export class PledgeApp extends PureComponent<Props> {
           <Fragment>
             <MobileHeader history={history} state={state} />
             <MobileNavbar status={state.status} />
+            <PullToRefreshSpinner />
           </Fragment>
         ) : (
           <Sidebar
