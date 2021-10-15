895754eb3cbb577c48e8ecbf5c207d6d021cb9be
jwyan <50184113+jwyan@users.noreply.github.com>
2021-10-14 18:28:39 -0700

    Update data.js

--- client/src/containers/PledgeApp/components/Contacts/data.js
+++ client/src/containers/PledgeApp/components/Contacts/data.js
@@ -5,9 +5,9 @@ export default {
     'Theta', 'Iota', 'Kappa', 'Lambda',
     'Mu', 'Nu', 'Xi', 'Omicron',
     'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon',
-    'Phi'
+    'Phi', 'Chi', 'Psi'
   ],
-  active: ['Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi'],
+  active: ['Tau', 'Upsilon', 'Phi', 'Chi', 'Psi'],
   major: [
     'Aerospace Engineering', 'Bioengineering', 'Chemical Engineering',
     'Computer Engineering', 'Computer Science', 'Cognitive Science',
