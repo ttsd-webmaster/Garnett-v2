d6bac3ee3b173ce615489541c7d74ff6af36c1b8
desudhanvi <dhanvi.desu@imprint.co>
2023-04-11 14:50:55 -0700

    initiate ADs and pledge AEs

--- scripts/PledgeApp/Before/resetFirebase.js
+++ scripts/PledgeApp/Before/resetFirebase.js
@@ -1,20 +1,41 @@
 const admin = require("firebase-admin");
 require('dotenv').config({ path: `${process.env.HOME}/Projects/React/Garnett/.env` });
 
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+
 admin.initializeApp({
   credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+    type: FIREBASE_TYPE,
+    project_id: FIREBASE_PROJECT_ID,
+    private_key_id: FIREBASE_PRIVATE_KEY_ID,
+    private_key: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: FIREBASE_CLIENT_EMAIL,
+    client_id: FIREBASE_CLIENT_ID,
+    auth_uri: FIREBASE_AUTH_URI,
+    token_uri: FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL
   }),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+  databaseURL: FIREBASE_DATABASE_URL
 })
 
 const meritsRef = admin.database().ref('/merits');
--- scripts/PledgeApp/Before/uploadPledges.js
+++ scripts/PledgeApp/Before/uploadPledges.js
@@ -4,21 +4,42 @@ const urlExists = require('url-exists');
 const admin = require('firebase-admin');
 require('dotenv').config({ path: `${process.env.HOME}/Projects/React/Garnett/.env` });
 
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+
 admin.initializeApp({
   credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+    type: FIREBASE_TYPE,
+    project_id: FIREBASE_PROJECT_ID,
+    private_key_id: FIREBASE_PRIVATE_KEY_ID,
+    private_key: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: FIREBASE_CLIENT_EMAIL,
+    client_id: FIREBASE_CLIENT_ID,
+    auth_uri: FIREBASE_AUTH_URI,
+    token_uri: FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL
   }),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
-});
+  databaseURL: FIREBASE_DATABASE_URL
+})
 
 const bucket = admin.storage().bucket('garnett-230209.appspot.com');
 const usersRef = admin.database().ref('/users');
@@ -27,8 +48,8 @@ function uploadFile(file) {
   const userName = file.slice(0, -4);
   const names = userName.match(/[A-Z][a-z]+/g);
   const userRef = usersRef.child(userName);
-
-  bucket.upload(`pledgeImages/${file}`, {
+  console.log(names)
+  bucket.upload(`images/${file}`, {
     destination: file,
     gzip: true,
     metadata: {
@@ -36,14 +57,14 @@ function uploadFile(file) {
     }
   }).then(() => {
     const uploadedFile = bucket.file(file);
-    uploadedFile.getSignedUrl({ action: 'read', expires: '03-17-2025' })
+    uploadedFile.getSignedUrl({ action: 'read', expires: '03-17-2027' })
     .then((signedUrls) => {
       console.log(`Updated ${userName}'s photo`);
       userRef.update({
         firstName: names[0],
         lastName: names[1],
-        class: 'Phi',
-        email: 'phi@thetatau.org',
+        class: 'Alpha Epsilon',
+        email: 'alphaepsilon@thetatau.org',
         year: '1st Year',
         status: 'pledge',
         phone: '1234567890',
