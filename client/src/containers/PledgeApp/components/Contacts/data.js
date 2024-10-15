b87661a0353a052bac47a71ff141c59af84556ac
Simon Quach <simonquach04@gmail.com>
2024-10-15 13:42:26 -0700

    change picture script and add alpha eta class

--- client/src/containers/PledgeApp/components/Contacts/data.js
+++ client/src/containers/PledgeApp/components/Contacts/data.js
@@ -1,50 +1,126 @@
 export default {
   class: [
-    'Charter', 'Alpha', 'Beta', 'Gamma',
-    'Delta', 'Epsilon', 'Zeta', 'Eta',
-    'Theta', 'Iota', 'Kappa', 'Lambda',
-    'Mu', 'Nu', 'Xi', 'Omicron',
-    'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon',
-    'Phi', 'Chi', 'Psi', 'Alpha Beta', 'Alpha Gamma',
-    'Alpha Delta', 'Alpha Epsilon', 'Alpha Zeta'
+    "Charter",
+    "Alpha",
+    "Beta",
+    "Gamma",
+    "Delta",
+    "Epsilon",
+    "Zeta",
+    "Eta",
+    "Theta",
+    "Iota",
+    "Kappa",
+    "Lambda",
+    "Mu",
+    "Nu",
+    "Xi",
+    "Omicron",
+    "Pi",
+    "Rho",
+    "Sigma",
+    "Tau",
+    "Upsilon",
+    "Phi",
+    "Chi",
+    "Psi",
+    "Alpha Beta",
+    "Alpha Gamma",
+    "Alpha Delta",
+    "Alpha Epsilon",
+    "Alpha Zeta",
+    "Alpha Eta",
   ],
-  active: ['Chi', 'Psi', 'Alpha Beta', 'Alpha Gamma', 'Alpha Delta', 'Alpha Epsilon', 'Alpha Zeta'],
+  active: [
+    "Chi",
+    "Psi",
+    "Alpha Beta",
+    "Alpha Gamma",
+    "Alpha Delta",
+    "Alpha Epsilon",
+    "Alpha Zeta",
+    "Alpha Eta",
+  ],
   major: [
-    'Aerospace Engineering', 'Bioengineering', 'Chemical Engineering',
-    'Computer Engineering', 'Computer Science', 'Cognitive Science',
-    'Data Science', 'Electrical Engineering', 'Environmental Engineering',
-    'Math-Computer Science', 'Mechanical Engineering', 'Nanoengineering',
-    'Structural Engineering'
+    "Aerospace Engineering",
+    "Bioengineering",
+    "Chemical Engineering",
+    "Computer Engineering",
+    "Computer Science",
+    "Cognitive Science",
+    "Data Science",
+    "Electrical Engineering",
+    "Environmental Engineering",
+    "Math-Computer Science",
+    "Mechanical Engineering",
+    "Nanoengineering",
+    "Structural Engineering",
   ],
   name: [
-    'A','B','C','D','E','F','G','H','I','J','K','L','M',
-    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
   ],
-  year: ['1st Year', '2nd Year', '3rd Year', '4th Year', '5th Year', 'Alumni'],
+  year: ["1st Year", "2nd Year", "3rd Year", "4th Year", "5th Year", "Alumni"],
   mbti: [
-    'INTJ', 'INTP', 'ENTJ', 'ENTP',
-    'INFJ', 'INFP', 'ENFJ', 'ENFP',
-    'ISTJ', 'ISTP', 'ESTJ', 'ESTP',
-    'ISFJ', 'ISFP', 'ESFJ', 'ESFP'
-  ]
+    "INTJ",
+    "INTP",
+    "ENTJ",
+    "ENTP",
+    "INFJ",
+    "INFP",
+    "ENFJ",
+    "ENFP",
+    "ISTJ",
+    "ISTP",
+    "ESTJ",
+    "ESTP",
+    "ISFJ",
+    "ISFP",
+    "ESFJ",
+    "ESFP",
+  ],
 };
 
 export type Filter =
-   'active' |
-   'alumni' |
-   'class' |
-   'major' |
-   'year' |
-   'firstName' |
-   'lastName' |
-   'mbti';
+  | "active"
+  | "alumni"
+  | "class"
+  | "major"
+  | "year"
+  | "firstName"
+  | "lastName"
+  | "mbti";
 
 export type FilterName =
-   'Active' |
-   'Alumni' |
-   'Class' |
-   'Major' |
-   'Year' |
-   'First Name' |
-   'Last Name' |
-   'Personality Type';
+  | "Active"
+  | "Alumni"
+  | "Class"
+  | "Major"
+  | "Year"
+  | "First Name"
+  | "Last Name"
+  | "Personality Type";
