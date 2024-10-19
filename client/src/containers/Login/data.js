6ad0aa2dde0c83f50ae1b9cd3058d367c6f7085b
Simon Quach <simonquach04@gmail.com>
2024-10-18 21:10:32 -0700

    add alpha theta

--- client/src/containers/Login/data.js
+++ client/src/containers/Login/data.js
@@ -1,83 +1,110 @@
 export const formData1 = [
   {
-    placeholder: 'First Name',
-    name: 'firstName',
-    type: 'text'
+    placeholder: "First Name",
+    name: "firstName",
+    type: "text",
   },
   {
-    placeholder: 'Last Name',
-    name: 'lastName',
-    type: 'text'
+    placeholder: "Last Name",
+    name: "lastName",
+    type: "text",
   },
 ];
 
 export const selectData = [
   {
-    placeholder: 'Class',
-    name: 'className',
+    placeholder: "Class",
+    name: "className",
     options: [
-      'Charter', 'Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta',
-      'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi',
-      'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Alpha Beta', 
-      'Alpha Gamma', 'Alpha Delta', 'Alpha Epsilon', 'Alpha Zeta', 'Alpha Eta'
-    ]
+      "Charter",
+      "Alpha",
+      "Beta",
+      "Gamma",
+      "Delta",
+      "Epsilon",
+      "Zeta",
+      "Eta",
+      "Theta",
+      "Iota",
+      "Kappa",
+      "Lambda",
+      "Mu",
+      "Nu",
+      "Xi",
+      "Omicron",
+      "Pi",
+      "Rho",
+      "Sigma",
+      "Tau",
+      "Upsilon",
+      "Phi",
+      "Chi",
+      "Psi",
+      "Alpha Beta",
+      "Alpha Gamma",
+      "Alpha Delta",
+      "Alpha Epsilon",
+      "Alpha Zeta",
+      "Alpha Eta",
+      "Alpha Theta",
+    ],
   },
   {
-    placeholder: 'Major',
-    name: 'major',
+    placeholder: "Major",
+    name: "major",
     options: [
-      'Aerospace Engineering',
-      'Bioengineering',
-      'Chemical Engineering',
-      'Computer Engineering',
-      'Computer Science',
-      'Data Science',
-      'Electrical Engineering',
-      'Environmental Engineering',
-      'Math-Computer Science',
-      'Mechanical Engineering',
-      'Nanoengineering',
-      'Structural Engineering'
-    ]
+      "Aerospace Engineering",
+      "Bioengineering",
+      "Chemical Engineering",
+      "Computer Engineering",
+      "Computer Science",
+      "Data Science",
+      "Electrical Engineering",
+      "Environmental Engineering",
+      "Math-Computer Science",
+      "Mechanical Engineering",
+      "Nanoengineering",
+      "Structural Engineering",
+    ],
   },
   {
-    placeholder: 'Year',
-    name: 'year',
+    placeholder: "Year",
+    name: "year",
     options: [
-      '1st Year',
-      '2nd Year',
-      '3rd Year',
-      '4th Year',
-      '5th Year',
-      'Alumni'
-    ]
-  }
+      "1st Year",
+      "2nd Year",
+      "3rd Year",
+      "4th Year",
+      "5th Year",
+      "Alumni",
+    ],
+  },
 ];
 
 export const formData2 = [
   {
-    placeholder: 'Phone (Only enter digits)',
-    name: 'phone',
-    type: 'number'
+    placeholder: "Phone (Only enter digits)",
+    name: "phone",
+    type: "number",
   },
   {
-    placeholder: 'Email',
-    name: 'email',
-    type: 'email'
+    placeholder: "Email",
+    name: "email",
+    type: "email",
   },
   {
-    placeholder: 'Authorization Code',
-    name: 'code',
-    type: 'password'
+    placeholder: "Authorization Code",
+    name: "code",
+    type: "password",
   },
   {
-    placeholder: 'Password (Must be longer than 6 characters)',
-    name: 'password',
-    type: 'password'
+    placeholder: "Password (Must be longer than 6 characters)",
+    name: "password",
+    type: "password",
   },
   {
-    placeholder: 'Password Confirmation',
-    name: 'confirmation',
-    type: 'password'
+    placeholder: "Password Confirmation",
+    name: "confirmation",
+    type: "password",
   },
 ];
